dbcaea2a5e0bb9af1e5f73de5e096c305c49233c
Fierclash <49929727+Fierclash@users.noreply.github.com>
2021-01-29 19:04:14 -0800

    Started Player Movement

---
+++ Global Game Jam 2021/Assets/Resources/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ *  Movement Class
+ *      Allows movement between tile positions
+ * 
+ *  Public Members:
+ *      void MoveToTile(Vector3);
+ * 
+ */
+public class Movement : MonoBehaviour
+{
+    public float movementTime;
+    public Transform hostBody;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Moving");
+            MoveToTile(transform.position + Vector3.left);
+        }
+    }
+
+    public void MoveToTile(Vector3 position)
+    {
+        hostBody.position = Vector3.Lerp(hostBody.position, position, movementTime);
+    }
+}
