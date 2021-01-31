98252b189b5a6c591b8d89201428ca0df8bb49d7
Fierclash <49929727+Fierclash@users.noreply.github.com>
2021-01-31 11:12:04 -0800

    Added Timer and Timer UI

---
+++ Global Game Jam 2021/Assets/Scripts/DynamicBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicBar : MonoBehaviour
+{
+    public Slider bar;
+    public Gradient gradient;
+    public Image fill;
+    private const float MAX_VALUE = 1f;
+    private IEnumerator timerCoroutine;
+
+    void Awake()
+    {
+        bar.maxValue = MAX_VALUE;
+        UpdateSize(MAX_VALUE);
+    }
+
+    public void UpdateSize(float percent)
+    {
+        bar.value = percent;
+        fill.color = gradient.Evaluate(bar.normalizedValue);  
+    }
+}
---
+++ Global Game Jam 2021/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public DynamicBar timerBar;
+    public float time = 3f;
+    private IEnumerator timerCoroutine;
+
+    #region Time Functions
+    public void Tick()
+    {
+        StopCoroutine(timerCoroutine);
+        timerCoroutine = StartTimer();
+        StartCoroutine(timerCoroutine);
+    }
+
+    private IEnumerator StartTimer()
+    {
+        for (float i = 0f; i <= time; i += Time.deltaTime)
+        {
+            timerBar.UpdateSize(i / time);
+            yield return null;
+        }
+    }
+    #endregion
+}
