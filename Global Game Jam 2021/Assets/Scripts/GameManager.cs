deadc3ce93d8f4308a347ddac4c4938d045b480b
Fierclash <49929727+Fierclash@users.noreply.github.com>
2021-01-31 13:14:13 -0800

    Touch Ups on UI

---
+++ Global Game Jam 2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool IsPaused { get; set; }
+    public int levelScene;
+    [SerializeField] private int currentScene;
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        ResumeGame();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && currentScene != levelScene)
+            StartGame();
+    }
+
+    #region Pause
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        IsPaused = false;
+    }
+    #endregion
+
+    #region Load Game
+    public void StartGame()
+    {
+        currentScene = levelScene;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
